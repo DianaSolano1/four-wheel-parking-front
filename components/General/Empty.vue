e2016dfe964ebed33a059433491d642752c7993c
Diana Solano <dianasolano1110@gmail.com>
2021-09-13 22:41:10 -0500

    feat(empty) Adding empty component

---
+++ components/General/Empty.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="empty-view">
+    <v-img
+      class="mx-auto"
+      src="/empty.svg"
+      width="350px"
+      alt="Sin resultados"
+    />
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.empty-view {
+  p {
+    font-size: 1.4em;
+    text-align: center;
+    margin-top: 40px;
+  }
+}
+</style>
