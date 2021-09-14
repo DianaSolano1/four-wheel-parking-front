3e3714d19383067b846ed48e9e791ca36312f4e9
Diana Solano <dianasolano1110@gmail.com>
2021-09-13 22:43:09 -0500

    feat(dash) Adding dashboard module

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,153 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <h1>Dashboard - Ingresos por día</h1>
+
+    <v-card class="mt-5 py-4 px-12 elevation-0 mx-auto" max-width="1000">
+      <h2 class="mb-2">Petición:</h2>
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            Reglas
+            <template v-slot:actions>
+              <v-icon color="info"> mdi-order-bool-ascending-variant </v-icon>
+            </template>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <ol>
+              <li>
+                Debes tener cuidado con las comas, las comillas y las llaves.
+                Sin esto la petición te arrojará objeto mal formado
+              </li>
+              <li>Debes enviar un arreglo</li>
+              <li>
+                Cada elemento del arreglo debe ser un arreglo que contenga
+                cuatro elementos
+              </li>
+              <li>
+                Los sub arreglos deben contener la siguiente estructura: String,
+                String, any, Number, any. De lo contrario no se realizará
+                correctamente el conteo o la validación de los datos
+              </li>
+            </ol>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            Ejemplo de petición
+            <template v-slot:actions>
+              <v-icon color="info"> mdi-help-circle </v-icon>
+            </template>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <pre>{{ example }}</pre>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+      <v-form class="mt-4" ref="form" v-model="valid" lazy-validation>
+        <v-textarea
+          v-model="base"
+          class="elevation-0"
+          label="Arreglo"
+          placeholder="Ingresar el arreglo"
+          outlined
+          :rules="baseRules"
+        ></v-textarea>
+      </v-form>
+      <div class="mt-4">
+        <v-btn
+          color="success"
+          style="color: white"
+          class="text-capitalize text-weight-bold text-space-normal elevation-0"
+          :disabled="!valid"
+          @click="acceptForm"
+          >Generar objeto</v-btn
+        >
+      </div>
+    </v-card>
+
+    <v-card class="mt-5 py-4 px-12 elevation-0 mx-auto" max-width="1000">
+      <h2>Respuesta:</h2>
+      <pre>{{ responseData }}</pre>
+    </v-card>
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    base: '',
+    baseRules: [(v) => !!v || 'Debe ingresar el arreglo base'],
+    responseData: {},
+    example: [
+      ['2018-12-01', 'AM', 'ID123', 5000],
+      ['2018-12-01', 'AM', 'ID545', 7000],
+      ['2018-12-01', 'PM', 'ID545', 3000],
+      ['2018-12-02', 'AM', 'ID545', 7000],
+    ],
+  }),
+  methods: {
+    acceptForm() {
+      if (this.$refs.form.validate()) {
+        try {
+          const inputs = JSON.parse(this.base)
+
+          if (!Array.isArray(inputs)) {
+            alert('Debe escribir un array')
+            return
+          }
+
+          const validLength = inputs.every((i) => i.length === 4)
+          const validType = inputs.every(
+            (i) =>
+              typeof i[3] === 'number' &&
+              typeof i[1] === 'string' &&
+              typeof i[0] === 'string'
+          )
+
+          if (!(validLength && validType)) {
+            alert(
+              'Cada array adentro debe ser de 4 elementos. El cuarto elemento tiene que ser número y los dos primeros strings'
+            )
+            return
+          }
+
+          const grouped = {}
+
+          for (const input of inputs) {
+            if (grouped[input[0]]) {
+              const currentDate = grouped[input[0]]
+
+              if (currentDate[input[1]]) {
+                currentDate[input[1]] += input[3]
+              } else {
+                currentDate[input[1]] = input[3]
+              }
+            } else {
+              grouped[input[0]] = {
+                [input[1]]: input[3],
+              }
+            }
+          }
+
+          this.responseData = grouped
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.log('Falló al generar por:', error)
+
+          alert(
+            'Objeto mal formado. Por favor básese en el ejemplo para realizar la prueba'
+          )
+        }
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+pre {
+  background-color: #2d3039;
+  color: white;
+}
+</style>
